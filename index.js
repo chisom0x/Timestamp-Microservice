6b11ba5c17feb980a548f0ec05c54582e342ea44
chisom0x <onyenankiekelvin@gmail.com>
2023-02-27 20:37:38 -0800

    modified:   index.js 	modified:   package-lock.json 	modified:   package.json

--- index.js
+++ index.js
@@ -24,9 +24,26 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api", (req, res)=>{
+  const now = new Date();
+ res.json({unix: now.getTime(), utc: now.toUTCString()})
+});
+
+app.get("/api/:date", (req, res)=>{
+  const paramsDate = req.params.date;
+  const invaliddate = "Invalid Date";
+  const date = parseInt(paramsDate) < 10000
+  ? new Date(paramsDate)
+  : new Date(parseInt(paramsDate))
+
+  date.toString() === invaliddate
+  ? res.json({ error: invaliddate })
+  : res.json({ unix: date.valueOf(), utc: date.toUTCString() });
+  
+})
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(4000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
